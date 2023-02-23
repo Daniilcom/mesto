f5d005145bac3616f0e116e34358eb1a5da82614
daniilcom <dan19.95@mail.ru>
2023-02-23 11:49:24 +0300

    feat: добавить функцию редактирования профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,25 @@
 let editProfile = document.querySelector('.profile__edit')
-let popupEditProfile = document.querySelector('.popup-edit')
-let closePopupEditProfile = document.querySelector('.popup-edit__close')
+let formElement = document.querySelector('.popup-edit')
+let closeFormElement = document.querySelector('.popup-edit__close')
+let nameInput = formElement.querySelector('.popup-edit__input_user_name')
+let descriptionInput = formElement.querySelector(
+  '.popup-edit__input_user_description'
+)
+let profileName = document.querySelector('.profile__name')
+let profileDescription = document.querySelector('.profile__description')
 
-const popupToggle = function () {
-  popupEditProfile.classList.toggle('popup-edit__open')
+function togglePopup() {
+  formElement.classList.toggle('popup-edit__open')
 }
 
-editProfile.addEventListener('click', popupToggle)
-closePopupEditProfile.addEventListener('click', popupToggle)
+function handleFormSubmit(evt) {
+  evt.preventDefault()
+  let nameValue = nameInput.value
+  let descriptionValue = descriptionInput.value
+  profileName.textContent = nameValue
+  profileDescription.textContent = descriptionValue
+}
+
+editProfile.addEventListener('click', togglePopup)
+closeFormElement.addEventListener('click', togglePopup)
+formElement.addEventListener('submit', handleFormSubmit)
