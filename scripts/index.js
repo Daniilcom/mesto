466195633150980d3efa750adc62721250ee411f
daniilcom <dan19.95@mail.ru>
2023-03-04 01:27:18 +0300

    fix: редактирование кода после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 let editProfile = document.querySelector('.profile__edit')
-let formElement = document.querySelector('.popup-edit')
-let closeFormElement = document.querySelector('.popup-edit__close')
-let nameInput = formElement.querySelector('.popup-edit__input_user_name')
+let formElement = document.querySelector('.popup')
+let closeFormElement = document.querySelector('.popup__close')
+let nameInput = formElement.querySelector('.popup__input_user_name')
 let descriptionInput = formElement.querySelector(
-  '.popup-edit__input_user_description'
+  '.popup__input_user_description'
 )
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
 
 function togglePopup() {
-  formElement.classList.toggle('popup-edit__open')
+  formElement.classList.toggle('popup_open')
+  nameInput.value = profileName.textContent
+  descriptionInput.value = profileDescription.textContent
 }
 
 function handleFormSubmit(evt) {
