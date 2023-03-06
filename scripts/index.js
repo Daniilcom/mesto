dd8c464bdb6b639737ccd0154a3153564c6aa01f
daniilcom <dan19.95@mail.ru>
2023-03-07 00:12:43 +0300

    feat: добавить карточки при загрузке и форму для них

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,66 @@
-let editProfile = document.querySelector('.profile__edit')
-let formElement = document.querySelector('.popup')
-let closeFormElement = document.querySelector('.popup__close')
-let nameInput = formElement.querySelector('.popup__input_user_name')
-let descriptionInput = formElement.querySelector(
+const gallaryItems = document.querySelector('.gallary__items')
+const templateItem = document.querySelector('#gallary-item')
+const gallaryItem = templateItem.querySelector('.gallary__item')
+
+const editProfile = document.querySelector('.profile__edit')
+const formProfile = document.querySelector('#popup-edit')
+const closeFormProfile = formProfile.querySelector('.popup__close')
+const nameInput = formProfile.querySelector('.popup__input_user_name')
+const descriptionInput = formProfile.querySelector(
   '.popup__input_user_description'
 )
-let profileName = document.querySelector('.profile__name')
-let profileDescription = document.querySelector('.profile__description')
+const profileName = document.querySelector('.profile__name')
+const profileDescription = document.querySelector('.profile__description')
 
-function togglePopup() {
-  formElement.classList.toggle('popup_open')
+const addPlace = document.querySelector('.profile__add')
+const formPlace = document.querySelector('#popup-add')
+const closeformPlace = formPlace.querySelector('.popup__close')
+const namePlaceInput = formPlace.querySelector('.popup__input_place_name')
+const linkPlaceInput = formPlace.querySelector('.popup__input_place_link')
+
+const initialGallary = [
+  {
+    place: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    place: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    place: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    place: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    place: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    place: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+]
+
+const addItem = (place, link) => {
+  const contentItem = templateItem.content
+  const cloneItem = contentItem.cloneNode(true)
+  cloneItem.querySelector('.gallary__description').textContent = place
+  cloneItem.querySelector('.gallary__image').src = link
+  return cloneItem
+}
+const defaultGallary = () =>
+  initialGallary.map((item) => {
+    gallaryItems.append(addItem(item.place, item.link))
+  })
+
+defaultGallary()
+
+const togglePopup = (popup) => {
+  popup.classList.toggle('popup_open')
   nameInput.value = profileName.textContent
   descriptionInput.value = profileDescription.textContent
 }
@@ -22,6 +73,13 @@ function handleFormSubmit(evt) {
   profileDescription.textContent = descriptionValue
 }
 
-editProfile.addEventListener('click', togglePopup)
-closeFormElement.addEventListener('click', togglePopup)
-formElement.addEventListener('submit', handleFormSubmit)
+// popup-edit
+
+editProfile.addEventListener('click', () => togglePopup(formProfile))
+closeFormProfile.addEventListener('click', () => togglePopup(formProfile))
+formProfile.addEventListener('submit', handleFormSubmit)
+
+//popup-add
+
+addPlace.addEventListener('click', () => togglePopup(formPlace))
+closeformPlace.addEventListener('click', () => togglePopup(formPlace))
